767ab8605cd32898131b064a0d597601d95b5722
san k <sankdev0@gmail.com>
2021-07-31 16:54:08 +0500

    Add use examples for kernel32 and C library
    Add developer process description to README

--- README.md
+++ README.md
@@ -10,3 +10,27 @@ JNA allows you to call directly into native functions using natural Java method 
 
 Many projects make use of JNA. For further information and use cases look in [official JNA page](https://github.com/java-native-access/jna)
 
+
+Development process
+-------------------
+
+Identify a native target library that you want to use. This can be any shared library with exported functions. Many examples of mappings for common system libraries, especially on Windows, may be found in the jna-platform package.
+
+Make your target library available to your Java program:
+- set the jna.library.path system property to the path to your target library;
+- change path environment variable before launching the VM;
+- make your native library available on your classpath, under the path {OS}-{ARCH}/{LIBRARY}, where {OS}-{ARCH} is JNA's canonical prefix for native libraries (e.g. win32-x86, linux-amd64, or darwin). If the resource is within a jar file it will be automatically extracted when loaded.
+
+Declare a Java interface to hold the native library methods by extending the Library interface.
+
+Within this interface, define an instance of the native library using the Native.load(Class) method, providing the native library interface you defined previously.
+
+Declare methods that mirror the functions in the target library by defining Java methods with the same name and argument types as the native function (refer to the basic mappings below or the detailed table of type mappings). You may also need to declare native structures to pass to your native functions. To do this, create a class within the interface definition that extends Structure and add public fields (which may include arrays or nested structures).
+
+Then invoke methods on the library instance just like any other Java class.
+
+*Example of such usage can be found in com.sankdev.examples.DemoKernel32Load.
+
+Optimized Direct Mapping
+------------------------
+JNA provides an additional method of mapping native methods, that can improve performance.  You may declare a class to hold your native methods, declare any number of methods with the `native` qualifier, and invoke `Native.register(String)` in the class static initializer with your library's name. See [JNA Direct Mapping](https://github.com/java-native-access/jna/blob/master/www/DirectMapping.md) for an example.
---
+++ src/main/java/com/sankdev/examples/DemoKernel32Load.java
@@ -0,0 +1,66 @@
+package com.sankdev.examples;
+
+import com.sun.jna.Native;
+import com.sun.jna.Structure;
+import com.sun.jna.win32.StdCallLibrary;
+
+public class DemoKernel32Load {
+
+    // kernel32.dll uses the __stdcall calling convention (check the function
+    // declaration for "WINAPI" or "PASCAL"), so extend StdCallLibrary
+    // Most C libraries will just extend com.sun.jna.Library.
+    public interface Kernel32 extends StdCallLibrary {
+        // Method declarations,constant and structure definitions go here.
+
+        // define an instance of the native library using the
+        // Native.load(Class) method, providing the native library interface
+        // you defined previously.
+        // The INSTANCE variable is for convenient reuse of a single
+        // instance of the library. Alternatively, you can load the
+        // library into a local variable so that it will be available
+        // for garbage collection when it goes out of scope. A Map of
+        // options may be provided as the third argument to load to
+        // customize the library behavior.
+        Kernel32 INSTANCE = (Kernel32) Native.load("kernel32", Kernel32.class);
+
+        // Optional: wraps every call to the native library in a
+        // synchronized block, limiting native calls to one at a time.
+        Kernel32 SYNC_INSTANCE = (Kernel32) Native.synchronizedLibrary(INSTANCE);
+
+        // Declare methods that mirror the functions in the target
+        // library by defining Java methods with the same name and
+        // argument types as the native function. You may also need
+        // to declare native structures to pass to your native functions.
+        // To do this, create a class within the interface definition
+        // that extends Structure and add public fields (which may
+        // include arrays or nested structures).
+
+        @Structure.FieldOrder({ "wYear", "wMonth", "wDayOfWeek", "wDay", "wHour", "wMinute", "wSecond", "wMilliseconds" })
+        public static class SYSTEMTIME extends Structure {
+            public short wYear;
+            public short wMonth;
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            public short wMilliseconds;
+        }
+
+        void GetSystemTime(SYSTEMTIME result);
+
+    }
+
+
+    // You can now invoke methods on the library instance just like any
+    // other Java class.
+    public static void main(String[] args) {
+
+        Kernel32 lib = Kernel32.INSTANCE;
+
+        Kernel32.SYSTEMTIME time = new Kernel32.SYSTEMTIME();
+        lib.GetSystemTime(time);
+
+        System.out.println("Today's integer value is " + time.wDay);
+    }
+}
